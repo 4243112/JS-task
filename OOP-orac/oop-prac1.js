61c305175100984f662d32ec51c18d2bee7d0508
4243112 <t.sato.sys24@morijyobi.ac.jp>
2024-12-18 11:54:23 +0900

    OOP-prac

--- OOP-orac/oop-prac1.js
+++ OOP-orac/oop-prac1.js
@@ -1,1 +1,19 @@
 "use strict";
+// Personクラスの定義
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`私は${this.name}です。${this.age}歳です。`);
+    }
+}
+
+const person1 = new Person('Taro', 20);
+const person2 = new Person('Hanako', 30);
+
+
+person1.introduce();
+person2.introduce();
--- OOP-orac/oop-prac3.js
+++ OOP-orac/oop-prac3.js
@@ -1,1 +1,31 @@
 "use strict";
+
+class Shape {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super(width, height);
+    }
+}
+
+class Square extends Shape {
+    constructor(sideLength) {
+        super(sideLength, sideLength);
+    }
+}
+
+const rectangle = new Rectangle(5, 10);
+const square = new Square(10);
+
+
+console.log(`縦5と横10の面積：${rectangle.area()}`);  
+console.log(`縦10と横10の面積：${square.area()}`);     
